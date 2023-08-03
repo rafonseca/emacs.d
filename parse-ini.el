c2457188d4969c057977789b9299a858e3f87ed2
rafonseca <renanfonseca@gmail.com>
2023-08-03 14:07:20 +0200

    make parse-ini-file a macro instead of function

--- parse-ini.el
+++ parse-ini.el
@@ -2,14 +2,15 @@
 (require 'each-match)
 
 
-(defun parse-ini-file (filename)
-  (erx-loop :rx '(: "[" (let heading (* nonl)) "]\n" (let settings (* (: (not "[") (* nonl) "\n"))) )
-	    :file filename
+(defmacro parse-ini-file (filename)
+  `(erx-loop :rx '(: "[" (let heading (* nonl)) "]\n" (let settings (* (: (not "[") (* nonl) "\n"))) )
+	    :file ,filename
 	    :do (cons *heading
-		      (erx-loop :file filename
+		      (erx-loop :file ,filename
 				:narrow settings
 				:rx '(: (let key (* nonl)) "=" (let value (* nonl)) "\n")
 				:do (cons *key *value)))))
+
 
 (ert-deftest parse-ini-file ()
   (should (parse-ini-file "../.pg_service.conf")))
